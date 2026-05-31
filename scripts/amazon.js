7053acdf26025a5ab4ba64b487597c62020ccb8f
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-05-31 19:07:06 +0530

    modules expoerted and imported from the files import products from products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-import {cart} from '../data/cart.js';
+import {cart,addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -57,30 +58,18 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
-    button.addEventListener('click',() => {
-        const productId = button.dataset.productId;
-        
-        let matchingItem;
-
-        cart.forEach((item)=>{
-            if(item.productId === productId){
-                matchingItem = item;
-            }
-        });
-
-        if(matchingItem){
-            matchingItem.quantity++;
-        }else{
-            cart.push({
-                productId: productId,
-                quantity: 1
-            });
-        }
-        let cartQuantity = 0;
+function updateCartQuantity(){
+    let cartQuantity = 0;
         cart.forEach((item)=>{
             cartQuantity += item.quantity;
         });
         document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+    button.addEventListener('click',() => {
+        const productId = button.dataset.productId;
+        addToCart(productId);
+        updateCartQuantity();
     });
 });
