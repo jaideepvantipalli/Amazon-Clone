aafb5dddca7b3144e53dda14185483a388579b04
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-06-01 00:26:49 +0530

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,8 @@ cart.forEach((cartItem)=>{
     });
 
     cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container 
+        js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -97,5 +98,8 @@ document.querySelectorAll('.js-delete-link').forEach((deleteLink)=>{
     deleteLink.addEventListener('click',()=>{
         const productId = deleteLink.dataset.productId;
         removeFromCart(productId);
+
+        const container=document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     });
 });
