dc2e5247fab6b1bd577e8580c80b21ce49c46a64
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-06-03 22:55:24 +0530

    learned error handling

--- data/products.js
+++ data/products.js
@@ -74,6 +74,8 @@ export  function loadProductsFetch(){
       return new Product(productDetails);
     });
     console.log('load products');
+  }).catch((error)=>{
+    console.log('Unexpected error.please try again later.');
   });
   return promise;
 }
@@ -100,9 +102,14 @@ export function loadProducts(fun){
     fun();
     
   });
+
+  xhr.addEventListener('error',(error)=>{
+    console.log('Unexpected error.please try again later.');
+  });
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
+loadProducts();
 
 /*
 export const products = [
