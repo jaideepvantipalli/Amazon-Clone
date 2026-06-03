ce6003f1d296297a301c00af85c2f787a3690f56
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-06-03 22:02:40 +0530

    learned promises and convert code to promises

--- data/products.js
+++ data/products.js
@@ -65,15 +65,17 @@ export function loadProducts(fun){
   const xhr=new XMLHttpRequest();
 
   xhr.addEventListener('load',()=>{
+    
     products = JSON.parse(xhr.response).map((productDetails)=>{
+
       if(productDetails.type === "clothing"){
         return new Clothing(productDetails);
       }
       return new Product(productDetails);
     });
-    console.log(products);
-
+    console.log('load products');
     fun();
+    
   });
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,34 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practise.js';
 
-loadProducts(()=>{
+Promise.all([
+    new Promise((resolve)=>{
+        loadProducts(()=>{
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    })
+]).then((value)=>{
+    console.log(value);
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+
+
+/*
+loadProducts(()=>{
+    loadCart(()=>{
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+});
+*/
