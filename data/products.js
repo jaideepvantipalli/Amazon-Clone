149460aa61ff3ba8d15051fd48e44ec0251f2f19
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-06-03 16:53:49 +0530

    use inheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -30,9 +30,49 @@ class Product{
   getPrice(){
     return `$${formatCurrency(this.priceCents)}`;
   }
-
+  extraInfoHTML(){
+    return '';
+  }
 }
 
+class Clothing extends Product{
+  sizeChartLink;
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+  //super.extraInfoHTML() to call the parent method and get the default empty string, then add the size chart link HTML to it and return the combined string
+  extraInfoHTML(){
+    return `
+      <div class="size-chart-link-container">
+        <a href="${this.sizeChartLink}" target="_blank" class="link-primary">
+          Size Chart
+        </a>
+      </div>
+    `;
+  }
+}
+
+const tshirt=new Clothing({
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 799,
+    keywords: [
+      "tshirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+  });
+
+  console.log(tshirt);
+  console.log(tshirt.getPrice());
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -693,5 +733,8 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+  if(productDetails.type === "clothing"){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
